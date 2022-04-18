6624fc589939c09470f2bd1f028aa85c9ec70d4f
Yessica Gomez Sanchez <yegs1995@gmail.com>
2022-04-17 19:45:45 -0500

    update validation two test

--- cypress/integration/apiTests/validationTwo.js
+++ cypress/integration/apiTests/validationTwo.js
@@ -1,21 +1,23 @@
 /// <reference types="cypress" />
 
+const user = require('../../fixtures/createUser.json')
 describe('Validation Two',()=>{
-    const d = new Date();
-    let day = d.getDate()+1;
+
+    var today = new Date().toISOString().slice(0, 10);
+  
     it ('create user',()=>{
 
         cy.request({
             method: 'POST',
             url:'https://reqres.in/api/users',
             body:{
-                "name": "morpheus",
-                "job": "leader"
+                "name": user.name,
+                "job": user.job
             }
         }).then((resp) => {
             expect(resp.status).to.eq(201)
-            cy.log(resp.body.data)
-            expect(Date.parse(resp.body.createdAt)).to.eq(Date.parse(d))
+            const resDate = new Date(resp.body.createdAt);
+            expect(resDate.toISOString().slice(0, 10)).to.eq(today)
           })
 
     })
